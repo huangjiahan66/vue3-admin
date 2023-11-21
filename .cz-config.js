4904fb4b15de04dbaea36205ed1dcfd08256b8ab
huangjiahan66 <1198434221@qq.com>
2023-11-21 22:33:14 +0800

    feat(custom): x

--- .cz-config.js
+++ .cz-config.js
@@ -21,11 +21,11 @@ module.exports = {
     customScope: '请输入修改范围(可选):',
     subject: '请简要描述提交(必填):',
     body: '请输入详细描述(可选):',
-    footer: '请输入要关闭的issue(可选):',
+    footer: '请输入要关闭的issue(可选):', 
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
   // 跳过问题
   skipQuestions: ['body', 'footer'],
   // subject文字长度默认是72
   subjectLimit: 72
-}
+} 
