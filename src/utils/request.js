af4fe95a5e965a81ca9768f26c4cfcbe3dc2bf74
huangjiahan66 <1198434221@qq.com>
2023-11-29 12:45:51 +0800

    feat(custom): 新增响应拦截器封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,6 @@
+import { Message } from '@element-plus/icons'
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
@@ -9,4 +11,21 @@ service.interceptors.request.use((config) => {
   config.headers.icode = '0CF892BFA5EBE38E'
   return config
 })
+
+service.interceptors.response.use(
+  (response) => {
+    const { success, message, data } = response.data
+    if (success) {
+      return data
+    } else {
+      ElMessage.error(message)
+      return Promise.reject(new Error(message))
+    }
+  },
+  // 404 500 请求失败
+  (error) => {
+    Message.error(error)
+    return Promise.reject(new Error(error))
+  }
+)
 export default service
